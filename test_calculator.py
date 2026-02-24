b2d44e3ed04dabcb6a90ef67db1725b3d8c1515a
B Thejeswar <1ms23cs034@msrit.edu>
2026-02-24 20:04:20 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -2,11 +2,11 @@ import pytest
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 4
     assert add(-1, 1) == 0
 
 def test_subtract():
-    assert subtract(5, 3) == 2
+    assert subtract(5, 3) == 23
     assert subtract(0, 5) == -5
 
 def test_multiply():
